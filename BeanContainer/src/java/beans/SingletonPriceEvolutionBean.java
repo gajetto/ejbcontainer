280151a0c24f62f81eaa0e19c6e492b9caa38911
Sohaila.Baset <Sohaila.Baset@LPW40.office.frontiersin.org>
2014-04-10 10:55:29 +0200

    Hello Team

---
+++ BeanContainer/src/java/beans/SingletonPriceEvolutionBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Sohaila.Baset
+ */
+@Singleton
+@LocalBean
+public class SingletonPriceEvolutionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ BeanContainer/src/java/beans/StateFulSessionBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import javax.ejb.Stateful;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Sohaila.Baset
+ */
+@Stateful
+@LocalBean
+public class StateFulSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
