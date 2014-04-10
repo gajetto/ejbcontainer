d405c08d9ebf23a6587d1cea07f40a6b8abc1cf8
Sohaila.Baset <Sohaila.Baset@LPW40.office.frontiersin.org>
2014-04-10 11:44:10 +0200

    Hello Team

--- BeanContainer/src/java/entities/User.java
+++ BeanContainer/src/java/entities/User.java
@@ -7,21 +7,47 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
 
 /**
  *
  * @author Sohaila.Baset
  */
 @Entity
+@Table(name = "User")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "UserId", nullable = false)    
     private Long id;
+    
+    @Column(name = "UserName")
+    private String userName;
+    
+    @Column(name = "FirstName")
+    private String firstName;
+    
+    @Column(name = "LastName")
+    private String lastName;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @Column(name = "DOB")
+    private Date dOB;
+    
+    @Column(name = "Password")
+    private String password;
+    
+    @Column(name = "IsAdmin")
+    private boolean isAdmin;
+    
 
     public Long getId() {
         return id;
@@ -54,6 +80,90 @@ public class User implements Serializable {
     @Override
     public String toString() {
         return "beans.User[ id=" + id + " ]";
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the isAdmin
+     */
+    public boolean isIsAdmin() {
+        return isAdmin;
+    }
+
+    /**
+     * @param isAdmin the isAdmin to set
+     */
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    /**
+     * @return the dOB
+     */
+    public Date getdOB() {
+        return dOB;
+    }
+
+    /**
+     * @param dOB the dOB to set
+     */
+    public void setdOB(Date dOB) {
+        this.dOB = dOB;
     }
     
 }
